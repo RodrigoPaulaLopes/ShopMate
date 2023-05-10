4f411eb9c72db21c27afcd6b92f13fc7ad23bc4c
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2023-05-10 00:46:47 -0300

    criando o projeto

---
+++ src/main/java/com/rodrigo/ShopMate/ShopMateApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.ShopMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/ShopMate/ShopMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.ShopMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
