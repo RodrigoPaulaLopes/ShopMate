04424b371a0d4d736376dac0e588f9b93a8115a8
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2023-05-14 11:21:29 -0300

    resolvendo erro no dto de produtos

--- src/main/java/com/rodrigo/ShopMate/dtos/ListCategoryDto.java
+++ src/main/java/com/rodrigo/ShopMate/dtos/ListCategoryDto.java
@@ -2,9 +2,14 @@ package com.rodrigo.ShopMate.dtos;
 
 import com.rodrigo.ShopMate.entities.Category;
 
+import java.util.List;
+import java.util.Set;
+
 public record ListCategoryDto(Long id, String nome) {
 
-    public ListCategoryDto(Category category){
+    public ListCategoryDto(Category category) {
         this(category.getId(), category.getName());
     }
+
+
 }
--- src/main/java/com/rodrigo/ShopMate/dtos/ListProductDto.java
+++ src/main/java/com/rodrigo/ShopMate/dtos/ListProductDto.java
@@ -3,12 +3,13 @@ package com.rodrigo.ShopMate.dtos;
 import com.rodrigo.ShopMate.entities.Category;
 import com.rodrigo.ShopMate.entities.Product;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public record ListProductDto(Long id, String name, String description, Double price, String imgUri, Set<Category> categories) {
+public record ListProductDto(Long id, String name, String description, Double price, String imgUri, Set<ListCategoryDto> categories) {
 
     public ListProductDto(Product product){
-        this(product.getId(), product.getName(), product.getDescription(), product.getPrice(), product.getImgUri(), new HashSet<>());
+        this(product.getId(), product.getName(), product.getDescription(), product.getPrice(), product.getImgUri(), new HashSet<>(product.getCategories().stream().map(ListCategoryDto::new).toList()));
     }
 }
